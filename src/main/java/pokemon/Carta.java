515bfbfef193b4d0594e974a0a4df03880ff5f28
QTeofilo <David.ortega@cunef.edu>
2026-03-06 19:52:47 +0100

    Creation of methods aprobe and reject

--- src/main/java/pokemon/Carta.java
+++ src/main/java/pokemon/Carta.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Carta {
+
+    public enum EstadoC{
+        DISPONIBLE,
+        RESERVADA,
+        NO_INTERCAMBIABLE
+    }
+
     public int idCarta;
     public String dueno;
     public String nombre;
     public String tipo;
     public int puntuacion;
-    public String estado;
+    public EstadoC estado;
     public Date fechaAlta;
 
     // Constructor vacío
--- src/main/java/pokemon/Solicitud.java
+++ src/main/java/pokemon/Solicitud.java
@@ -3,11 +3,10 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Solicitud {
 
     // Estados permitidos
-    public enum Estado{
+    public enum EstadoS{
         PENDIENTE,
         ACEPTADO,
         RECHAZADO
@@ -20,7 +19,7 @@ public class Solicitud {
     public int idCarta2;
     public String dueno2;
     public Date fechaSolicitud;
-    public Estado estado;
+    public EstadoS estado;
 
 
     // Constructor para crear una solicitud
@@ -43,7 +42,71 @@ public class Solicitud {
         this.estado = estado;
         this.fechaSolicitud = fechaSolicitud;
     }
-    
+
+    public void aceptar(){
+
+        if (estado == EstadoS.PENDIENTE){
+            estado = EstadoS.ACEPTADO;
+
+            carta1 = buscarCartaPorId(idCarta1);
+            carta2 = buscarCartaPorId(idCarta2);
+            if (carta1 == null || carta2 == null) {
+                System.out.println("Error: alguna carta no existe");
+                return;
+            }
+
+            carta1.dueno = dueno2;
+            carta2.dueno = dueno1;
+
+            carta1.estado = EstadoC.DISPONIBLE;
+            carta2.estado = EstadoC.DISPONIBLE;
+
+            control *= carta1.modificarCarta();
+            control *= carta2.modificarCarta();
+
+            if(control){
+                System.out.println("Intercambio realizado correctamente");
+            }
+            else{
+                System.out.println("Intercambio no realizado, ha ocurrido un error con la BD");
+            }
+        }
+        else {
+            System.out.println("Error: la solicitud ya se ha resuelto");
+            return;
+        }
+    }
+
+    public void rechazar(){
+        if (estado == EstadoS.PENDIENTE){
+            estado = EstadoS.RECHAZADO;
+
+            carta1 = buscarCartaPorId(idCarta1);
+            carta2 = buscarCartaPorId(idCarta2);
+            if (carta1 == null || carta2 == null) {
+                System.out.println("Error: alguna carta no existe");
+                return;
+            }
+
+            carta1.estado = EstadoC.DISPONIBLE;
+            carta2.estado = EstadoC.DISPONIBLE;
+
+            control1 = carta1.modificarCarta();
+            control2 = carta2.modificarCarta();
+
+            if(control1 && control2){
+                System.out.println("Intercambio realizado correctamente");
+            }
+            else{
+                System.out.println("Intercambio no realizado, ha ocurrido un error con la BD");
+            }
+        }
+        else {
+            System.out.println("Error: la solicitud ya se ha resuelto");
+            return;
+        }
+    }
+
     @Override
     public String toString() {
         return "Solicitud{" +
